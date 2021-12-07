9f0906a89d049b94374fd4855da127941c25e2a8
rflorian <rflorian@github.com>
2021-12-07 14:19:48 +0100

    rust experimenting

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-mod solutions;
 use std::{ops::Sub, time::Instant};
+
+mod solutions;
 
 macro_rules! run_day {
     ($($d:ident),*) => {
@@ -13,9 +14,19 @@ macro_rules! run_day {
 }
 
 fn main() {
+    // prevent dead code errors
     if 0 == 1 {
         run_day!(day1, day2);
     }
+
+    let args: Vec<String> = std::env::args().collect();
+    println!("Got {:?} arguments: {:?}.", args.len() - 1, &args[1..]);
+
+    if args.len() < 2 {
+         panic!("Missing argument for day")
+    }
+    let day = &args[1].parse::<i32>().unwrap();
+    println!("Day is {}", day);
 
     let start = Instant::now();
     run_day!(day1);
