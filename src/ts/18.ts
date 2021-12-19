866fa0a20124eac14d047dd01121fcf8a8e1fc3b
rflorian <rflorian@github.com>
2021-12-19 17:43:16 +0100

    day 18 cleanup

--- src/ts/18.ts
+++ src/ts/18.ts
@@ -1,36 +1,30 @@
 export default (rawInput: string) => {
     type Arrayfish = [left: Arrayfish, right: Arrayfish] | number;
-    type Snailfish<T = unknown> = {left: Snailfish<T>, right: Snailfish<T>;} & T | number;
+    type Snailfish = {left: Snailfish, right: Snailfish;} | number;
 
-    const toArray = (arrayFish: Arrayfish): Snailfish => {
-        if (typeof arrayFish === 'number') return arrayFish;
-        return {left: toArray(arrayFish[0]), right: toArray(arrayFish[1])};
-    };
+    const toArray = (arrayFish: Arrayfish): Snailfish =>
+        typeof arrayFish === 'number'
+            ? arrayFish
+            : {left: toArray(arrayFish[0]), right: toArray(arrayFish[1])};
 
     type Node = [number, number];
-    const parse = (snail: Snailfish, depth: number): Node[] => {
-        if (typeof snail === 'number') {
-            return [[snail, depth]];
-        }
-
-        return [
-            ...parse(snail.left, depth + 1),
-            ...parse(snail.right, depth + 1),
-        ];
-    };
+    const parse = (snail: Snailfish, depth: number): Node[] =>
+        typeof snail === 'number'
+            ? [[snail, depth]]
+            :
+            [
+                ...parse(snail.left, depth + 1),
+                ...parse(snail.right, depth + 1),
+            ];
 
     const reduce = (nodes: Node[]): Node[] => {
         const explodeIdx = nodes.findIndex(n => n[1] > 4);
         if (explodeIdx !== -1) {
-            if (explodeIdx > 0) { // element to the left
-                const addNode = nodes.filter((_, i) => i < explodeIdx).reverse()[0];
-                addNode[0] += nodes[explodeIdx][0];
-            }
+            // element to the left
+            if (explodeIdx > 0) nodes.filter((_, i) => i < explodeIdx).reverse()[0][0] += nodes[explodeIdx][0];
 
-            if (explodeIdx < nodes.length - 2) { // element to the right
-                const addNode = nodes.filter((_, i) => i > explodeIdx + 1)[0];
-                addNode[0] += nodes[explodeIdx + 1][0];
-            }
+            // element to the right
+            if (explodeIdx < nodes.length - 2) nodes.find((_, i) => i > explodeIdx + 1)[0] += nodes[explodeIdx + 1][0];
 
             nodes.splice(explodeIdx, 2, [0, nodes[explodeIdx][1] - 1] as Node);
             return reduce(nodes);
@@ -46,32 +40,34 @@ export default (rawInput: string) => {
         return nodes;
     };
 
-    const add = (a: Node[], b: Node[]): Node[] => [
+    const add = (a: Node[], b: Node[]) => [
         ...a.map(([v, d]) => [v, d + 1] as Node),
         ...b.map(([v, d]) => [v, d + 1] as Node),
     ];
 
     const snailfishNumbers = rawInput.split('\n').map(line => parse(toArray(eval(line)), 0));
 
-    type LinkedSnailfish = {left: LinkedSnailfish, right: LinkedSnailfish, parent: Exclude<LinkedSnailfish, number>;};
-    const newNode = (parent: Exclude<LinkedSnailfish, number>): LinkedSnailfish => ({left: null, right: null, parent});
     const reassemble = (nodes: Node[]): Snailfish => {
+        type LinkedSnailfish = {left: LinkedSnailfish, right: LinkedSnailfish, parent: Exclude<LinkedSnailfish, number>;};
+        const newNode = (parent: LinkedSnailfish): LinkedSnailfish => ({left: null, right: null, parent});
+
         const res: LinkedSnailfish = newNode(null);
         let ptr = res;
         let currentDepth = 0;
         for (const [value, depth] of nodes) {
-            while (currentDepth > 0) {
+            while (currentDepth > 0) { // find first unset ptr.right or go back to root
                 if (ptr.right == null) break;
                 currentDepth--;
                 ptr = ptr.parent;
             }
-            if (currentDepth + 1 < depth && ptr.left != null && ptr.right == null) {
+
+            if (currentDepth < depth - 1 && ptr.left != null) { // insert ptr.right if needed
                 ptr.right = newNode(ptr);
                 ptr = ptr.right;
                 currentDepth++;
             }
 
-            while (currentDepth + 1 < depth) { // travel down ptr.left until depth = targetDepth
+            while (currentDepth < depth - 1) { // travel down ptr.left until depth = targetDepth
                 currentDepth++;
                 ptr.left = newNode(ptr);
                 ptr = ptr.left;
@@ -84,26 +80,24 @@ export default (rawInput: string) => {
         return res;
     };
 
-    const magnitude = (fish: Snailfish): number => {
-        if (typeof fish === 'number') return fish;
-
-        return 3 * magnitude(fish.left) + 2 * magnitude(fish.right);
-    };
+    const magnitude = (fish: Snailfish): number => typeof fish === 'number' ? fish : 3 * magnitude(fish.left) + 2 * magnitude(fish.right);
 
     return [
         magnitude(reassemble(snailfishNumbers.reduce((acc, n) => acc ? reduce(add(acc, n)) : n))),
-        Math.max(...Array.from(
-            {length: snailfishNumbers.length},
-            (_, i) => Array.from(
-                {length: snailfishNumbers.length},
-                (_, j) =>
-                    i === j
-                        ? []
-                        : [
-                            magnitude(reassemble(reduce(add(snailfishNumbers[i], snailfishNumbers[j])))),
-                            magnitude(reassemble(reduce(add(snailfishNumbers[j], snailfishNumbers[i])))),
-                        ]
-            ),
-        ).flat(2)),
+        Math.max(
+            ...Array.from(
+                {length: snailfishNumbers.length - 1},
+                (_, i) => Array.from(
+                    {length: snailfishNumbers.length - 1},
+                    (_, j) =>
+                        i === j
+                            ? []
+                            : [
+                                magnitude(reassemble(reduce(add(snailfishNumbers[i], snailfishNumbers[j])))),
+                                magnitude(reassemble(reduce(add(snailfishNumbers[j], snailfishNumbers[i])))),
+                            ]
+                ),
+            ).flat(2),
+        ),
     ];
 };
