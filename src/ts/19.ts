6114fb36f7811b8d70927237629aaac4b1c922f0
rflorian <rflorian@github.com>
2021-12-25 17:25:28 +0100

    day 19 wip

--- src/ts/19.ts
+++ src/ts/19.ts
@@ -1,20 +1,76 @@
 export default (rawInput: string) => {
-    const scanners = [];
+    type Point = [x: number, y: number, z: number];
+    type Scanner = Point[];
+
+    const scanners = [] as Scanner[];
     for (const scanner of rawInput.split('\n\n')) {
-        let beacons: [number, number, number][] = [];
+        let points = [] as Point[];
 
         for (const line of scanner.split('\n')) {
             const res = line.match(/^(-?\d+),(-?\d+),?(-?\d+)?$/);
             if (!res) continue;
 
-            const [_, ...beacon] = res.map(Number)
-            beacons.push(beacon.filter(v => !isNaN(v)) as any);
+            const [_, ...beacon] = res.map(Number);
+            points.push(beacon.filter(v => !isNaN(v)) as any);
         }
 
-        scanners.push(beacons);
+        scanners.push(points);
     }
 
+    console.log(scanners);
+
+    type Rotation = (p: Point) => Point;
+    const rotations: Rotation[] = [
+        ([x, y, z]) => [x, y, z],
+        ([x, y, z]) => [y, z, x],
+        ([x, y, z]) => [z, x, y],
+        ([x, y, z]) => [-x, z, y],
+        ([x, y, z]) => [z, y, -x],
+        ([x, y, z]) => [y, -x, z],
+        ([x, y, z]) => [x, z, -y],
+        ([x, y, z]) => [z, -y, x],
+        ([x, y, z]) => [-y, x, z],
+        ([x, y, z]) => [x, -z, y],
+        ([x, y, z]) => [-z, y, x],
+        ([x, y, z]) => [y, x, -z],
+        ([x, y, z]) => [-x, -y, z],
+        ([x, y, z]) => [-y, z, -x],
+        ([x, y, z]) => [z, -x, -y],
+        ([x, y, z]) => [-x, y, -z],
+        ([x, y, z]) => [y, -z, -x],
+        ([x, y, z]) => [-z, -x, y],
+        ([x, y, z]) => [x, -y, -z],
+        ([x, y, z]) => [-y, -z, x],
+        ([x, y, z]) => [-z, x, -y],
+        ([x, y, z]) => [-x, -z, -y],
+        ([x, y, z]) => [-z, -y, -x],
+        ([x, y, z]) => [-y, -x, -z],
+    ];
+
+    const part1 = (scanners: Scanner[]): number => {
+        let oriented = [] as Scanner[];
+        oriented.push(scanners.shift());
+
+        while (scanners.length) {
+            scanners.forEach((scanner, idx) => {
+                for (const solved of oriented) {
+                    for (let i = 0; i < 24; i++) {
+                        const rotated = scanner.map(rotations[i]);
+                        console.log(rotated);
+
+
+                        if (false) {
+                            oriented.push(scanners.splice(idx, 1)[0]);
+                        }
+                    }
+                }
+            });
+        }
+
+        return 0;
+    };
+
     return [
-        1
+        part1(scanners),
     ];
 };
